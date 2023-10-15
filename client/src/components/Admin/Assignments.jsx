fc20519b7c98a9a1e62ac4cd0b95d902fa36803e
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-10-15 08:38:05 +0530

    Variable name changed and added.

--- client/src/components/Admin/Assignments.jsx
+++ client/src/components/Admin/Assignments.jsx
@@ -1,87 +1,163 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-
+import axios from "axios";
+import React, { useState } from "react";
 
 const Assignments = () => {
+  const [assignment, setAssignment] = useState({
+    title: "",
+    instructors: "",
+    type: "",
+    scheduled: "",
+    time: "",
+    link: "",
+    password: "",
+  });
 
-  const [assignment, setAssignment] = useState(
-    {
-        name:"",
-        creator:"",
-        type:"",
-        date:"",
-        time:"",
-        link:"",
-        password:""
-    }
-)
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { id, value } = e.target;
 
-const handleChange = (e) => {
-  e.preventDefault();
-    const {id, value} = e.target
-   
-    setAssignment({...assignment, [id]:value})
-}
+    setAssignment({ ...assignment, [id]: value });
+  };
 
-const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(assignment)
-    
-    axios.post('https://lms-app-123.herokuapp.com/assignment', assignment).then((res)=>{
-     
-      alert('Assignment added');
-      e.target.reset();
-      
-    }).catch((e)=>{
-      console.log(e.message)
-    })
- 
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(assignment);
 
+    axios
+      .post("http://localhost:8080/assignment", assignment)
+      .then((res) => {
+        alert(res.data.message);
+        e.target.reset();
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  };
 
   return (
     <>
-        <div className='mainBox'>
-          <h1 className='nameHead'>Assignments</h1>
-              <form onSubmit={handleSubmit}>
-
-                <div className='divideInput'>
-                  <div><label>Name :</label></div>
-                  <div><input type="text" className='inpBox' id='name' onInput={handleChange} required/></div>
-                </div>
-
-                <div className='divideInput'> 
-                  <div><label>Creator Name:</label></div>
-                  <div><input type="text" className='inpBox' id='creator'  onInput={handleChange} required /></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Type:</label></div>
-                  <div><input type="text" className='inpBox' id='type'  onInput={handleChange} required/></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Date:</label></div>
-                  <div><input type="Date" className='inpBox' id='date' onInput={handleChange} required /></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Time:</label></div>
-                  <div><input type="time" className='inpBox' id='time' onInput={handleChange} required /></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Link:</label></div>
-                  <div><input type="link" className='inpBox' id='link' onInput={handleChange} required /></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Password:</label></div>
-                  <div><input type="text" className='inpBox' id='password'  onInput={handleChange} required/></div>
-                </div>
-
-                <input type="submit" value="Submit" className='submitBtn'/>
-
-  
-              </form>
+      <div className="mainBox">
+        <h1 className="nameHead">Assignments</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="divideInput">
+            <div>
+              <label>Title :</label>
             </div>
-    </>
-  )
-}
+            <div>
+              <input
+                type="text"
+                className="inpBox"
+                id="title"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
 
-export default Assignments
+          <div className="divideInput">
+            <div>
+              <label>Instructors:</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                className="inpBox"
+                id="instructors"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Category ( Coding / DSA )</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="category"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Type ( Live / Video / Read)</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="type"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Scheduled:</label>
+            </div>
+            <div>
+              <input
+                type="Date"
+                className="inpBox"
+                id="scheduled"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Time:</label>
+            </div>
+            <div>
+              <input
+                type="time"
+                className="inpBox"
+                id="time"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Link:</label>
+            </div>
+            <div>
+              <input
+                type="link"
+                className="inpBox"
+                id="link"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Password:</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                className="inpBox"
+                id="password"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+
+          <input type="submit" value="Submit" className="submitBtn" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Assignments;
--- client/src/components/Admin/Lectures.jsx
+++ client/src/components/Admin/Lectures.jsx
@@ -1,78 +1,148 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Lectures = () => {
+  const [lecture, setLecture] = useState({
+    title: "",
+    instructors: "",
+    category: "",
+    type: "",
+    scheduled: "",
+    time: "",
+    link: "",
+  });
 
-    const [lecture, setLecture] = useState(
-        {
-            name:"",
-            creator:"",
-            topic:"",
-            date:"",
-            time:"",
-            link:""
-        }
-    )
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setLecture({ ...lecture, [id]: value });
+  };
 
-    const handleChange = (e) => {
-        const {id, value} = e.target;
-        setLecture({...lecture, [id]:value})
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(lecture);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(lecture)
-
-        axios.post('https://lms-app-123.herokuapp.com/lecture', lecture).then((res)=>{
-         console.log("res : " + res.data)
-          alert('Lecture added');
-          e.target.reset();
-        }).catch((e)=>{
-          console.log(e)
-        })
-    }
-
+    axios
+      .post("http://localhost:8080/lecture", lecture)
+      .then((res) => {
+        alert(res.data.message);
+        e.target.reset();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   return (
     <>
-        <div className='mainBox'>
-            <h1 className='nameHead'>Lectures</h1>
-              <form onSubmit={handleSubmit}>
-
-                <div className='divideInput'>
-                  <div><label>Topic Name :</label></div>
-                  <div><input type="text" id='name' className='inpBox' onInput={handleChange} required/></div>
-                </div>
-
-                <div className='divideInput'> 
-                  <div><label>Creator Name:</label></div>
-                  <div><input type="text" id='creator' className='inpBox'  onInput={handleChange} required /></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Topic ( Coding / DSA )</label></div>
-                  <div><input type="text" id='topic' className='inpBox'  onInput={handleChange} required/></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Date:</label></div>
-                  <div><input type="Date" id='date' className='inpBox'  onInput={handleChange} required /></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Time:</label></div>
-                  <div><input type="time" id='time' className='inpBox' onInput={handleChange} required /></div>
-                </div>
-                <div className='divideInput'>
-                  <div><label>Link:</label></div>
-                  <div><input type="link" id='link' className='inpBox'  onInput={handleChange} required/></div>
-                </div>
-
-                <input type="submit" value="Submit" className='submitBtn'  />
-
-  
-              </form>
+      <div className="mainBox">
+        <h1 className="nameHead">Lectures</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="divideInput">
+            <div>
+              <label>Title:</label>
             </div>
-    </>
-  )
-}
+            <div>
+              <input
+                type="text"
+                id="title"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
 
-export default Lectures
+          <div className="divideInput">
+            <div>
+              <label>Instructors:</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="instictors"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Category ( Coding / DSA )</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="category"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Type ( Live / Video / Read)</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="type"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Scheduled:</label>
+            </div>
+            <div>
+              <input
+                type="Date"
+                id="scheduled"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Time:</label>
+            </div>
+            <div>
+              <input
+                type="time"
+                id="time"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="divideInput">
+            <div>
+              <label>Link:</label>
+            </div>
+            <div>
+              <input
+                type="link"
+                id="link"
+                className="inpBox"
+                onInput={handleChange}
+                required
+              />
+            </div>
+          </div>
+
+          <input type="submit" value="Submit" className="submitBtn" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Lectures;
