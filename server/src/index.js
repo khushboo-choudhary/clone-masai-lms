90847ee9c01584f94a8436124ae5a644635db197
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-10-20 23:59:37 +0530

    cors added

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,30 @@
-const express = require('express')
-require('dotenv').config()
-const connect = require('./configs/db')
-const app = express()
-app.use(express.json())
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const connect = require("./configs/db");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const assignment = require('./controllers/assignments.controllers')
-const lecture = require('./controllers/lecture.controllers')
-const user = require('./controllers/user.controllers')
-const {register, login} = require('./controllers/auth.controllers')
+const assignment = require("./controllers/assignments.controllers");
+const lecture = require("./controllers/lecture.controllers");
+const user = require("./controllers/user.controllers");
+const { register, login } = require("./controllers/auth.controllers");
 
-app.use('/assignment', assignment)
-app.use('/lecture', lecture)
+app.use("/assignment", assignment);
+app.use("/lecture", lecture);
 
-app.post('/register',register)
-app.use('/findUser', user)
-app.post('/login', login)
+app.post("/register", register);
+app.use("/findUser", user);
+app.post("/login", login);
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-app.listen(port,async()=>{
-    try{
-        await connect()
-        console.log("listening on", port)
-    }
-    catch(err){
-        console.log(err.message)
-    }
-})
+app.listen(port, async () => {
+  try {
+    await connect();
+    console.log("listening on", port);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
